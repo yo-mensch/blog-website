2eb6a1ed3ea782df33691425ef6b622f33311d46
yo-mensch <rasa.j1225@gmail.com>
2021-03-15 17:19:21 +0200

    implemented boilerplate to about & contact pages

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ app.get("/",(req,res)=>{
   res.render("home",{homePar: homeStartingContent});
 })
 
+app.get("/about", (req,res)=>{
+  res.render("about", {aboutPar: aboutContent});
+})
+
+app.get("/contact", (req, res)=>{
+  res.render("contact", {contactPar: contactContent});
+})
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
