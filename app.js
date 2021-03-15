c8bf96ad1234d194c3d7df9d5788044209d9aa50
yo-mensch <rasa.j1225@gmail.com>
2021-03-15 17:04:30 +0200

    added ejs boilerplates

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/",(req,res)=>{
+  res.render("home",{homePar: homeStartingContent});
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
